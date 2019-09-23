da1a5eab94fcf2f1a64ed71e6264a7230caacb8b
christian williams <christian.williamsj@gmail.com>
2019-09-23 20:59:45 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -35,6 +35,12 @@ class Owner
     Cat.all
   end
   
-  
+  def buy_dog(name_of_dog)
+    @pets[:dogs] << Dog.new(name_of_dog)
+  end
+
+  def buy_cat(name_of_cat)
+    @pets[:cats] << Cat.new(name_of_cat)
+  end
 
  end
